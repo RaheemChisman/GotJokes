346f96cb394af2c7c47e36ddd44ed22b38ed42e2
Raheem Chisman <raheem.chisman@gmail.com>
2022-12-03 19:01:53 -0800

    Add project files.

---
+++ GotJokes/Controllers/JokesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
+using GotJokes.Models;
+
+namespace GotJokes.Controllers
+{
+    public class JokesController : Controller
+    {
+        public IActionResult Index(JokesModel.Rootobject model)
+        {
+
+            return View(model);
+        }
+
+        public IActionResult GetJokes(string setup, string delivery, int id)
+        {
+            var client = new RestClient("https://v2.jokeapi.dev/joke/Dark");
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            var data = JsonConvert.DeserializeObject<JokesModel.Rootobject>(response.Content);
+            Console.WriteLine(response.Content);
+            var model = new JokesModel.Rootobject()
+            {
+                id = data.id,
+                setup = data.setup,
+                delivery = data.delivery,
+            };
+
+            return RedirectToAction("Index", model);
+        }
+
+
+    }
+}
